c62d2d1d931ba9f6c00119b60f663680fac54ac5
ayavrin <nirvayathapa89@gmail.com>
2024-07-18 19:10:29 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const submit = document.querySelector(".submit");
+const ratingNumber=document.querySelectorAll(".btn");
+let checkClick=false;
+let index;
+ratingNumber.forEach((e,i,a)=>{
+    e.addEventListener("click",
+            function(){
+                checkClick=true;
+                index=i;
+            }
+        )
+
+
+})
+
+
+
+
+submit.addEventListener("click",
+    function(event){
+        event.preventDefault();
+        check();
+    }
+)
+function check(){
+    console.log(checkClick);
+    if(checkClick){
+        console.log("working");
+        const firstcontent=document.querySelector(".container");
+        firstcontent.classList.add("hide");
+        const btncontent=document.querySelector("#ratingMeasure");
+        btncontent.innerText=`You selected ${ratingNumber[index].innerText} out of 5`;
+        const seconddiv=document.querySelector(".afterSubmit");
+        seconddiv.classList.remove('hide')
+        seconddiv.classList.add("show");
+    }
+}
